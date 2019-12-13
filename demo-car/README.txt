17859faafae7891f928705bff53c44c2f534119a
asinha.tech@gmail.com <asinha.tech@gmail.com>
2019-12-13 21:53:24 +0530

    initial commit

--- demo-car/README.txt
+++
@@ -1,3 +0,0 @@
-A Pen created at CodePen.io. You can find this one at https://codepen.io/YusukeNakaya/pen/ZadZxL.
-
- I really like car!! :)
--- demo-car/js/appVariables.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const appVariables = {
-IOT_ENDPOINT: 'data.iot.us-east-1.amazonaws.com', 
-IDENTITY_POOL_ID: '<POOL_ID_OUTPUT_BY_CDK>', 
-REGION: 'us-east-1'
-};
